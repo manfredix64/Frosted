e65c477f9f1657c344c0e45ff7a2bc01a6cb3ac7
manfredix64 <60573289+manfredix64@users.noreply.github.com>
2024-02-07 19:06:22 +0200

    added test command

---
+++ index.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { token, prefix } = require('./config.json');
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildPresences,
+        GatewayIntentBits.MessageContent
+    ]
+})
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
+
+client.on('ready', () => {
+    console.log('good')
+})
+
+client.on('messageCreate', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(token)
